cc77b05a81009c1789b3ca18d8087c812ad1a43a
Mikhail Bushuev <mbushuev@uma.tech>
2022-02-06 23:15:22 +0300

    hotfix, version bump 0.1.1

--- Package.swift
+++ Package.swift
@@ -31,13 +31,13 @@ let package = Package(
         ),
         
         .binaryTarget(name: "AFNetworking",
-                      url: "https://gitlab.zxz.su/mbushuev/gidsdk.pod/-/raw/main/SwiftPM/releases/0.1.0/AFNetworking.xcframework.4.0.2.zip",
+                      url: "https://gitlab.zxz.su/mbushuev/gidsdk.pod/-/raw/main/SwiftPM/releases/0.1.1/AFNetworking.xcframework.4.0.2.zip",
                       checksum: "04cef0cecfb7caa66bd1bd8ef8d347ed47f67f0412ef508828823655218dc7e8"
         ),
         
         .binaryTarget(name: "GIDSDK",
-                      url: "https://gitlab.zxz.su/mbushuev/gidsdk.pod/-/raw/main/SwiftPM/releases/0.1.0/GIDSDK.xcframework.0.1.0.zip",
-                      checksum: "3ea12e3269cc083f7a95c500ddfbb4613191b13ad7dc7660cc8423ca26764f85"
+                      url: "https://gitlab.zxz.su/mbushuev/gidsdk.pod/-/raw/main/SwiftPM/releases/0.1.1/GIDSDK.xcframework.0.1.1.zip",
+                      checksum: "9e1d6a76f20506f6f027934e63afb736140c825cb7d3c0a89fbdd23c6a7fc056"
         ),
         
     ]
