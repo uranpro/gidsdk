50e415c3f0fe53a120f0c82fbd4f8647ca15dcd0
Бушуев Михаил Андреевич <mabushuev@gid.ru>
2026-05-18 23:24:06 +0300

    feat(SDK-2622): update checksum and version

--- Package.swift
+++ Package.swift
@@ -43,13 +43,13 @@ let package = Package(
        .binaryTarget(
            name: "KFPMobileKit",
            url: "https://nexus.gid.team/repository/sso-sdk-ios/gid/sdk/spm/KFPMobileKit.xcframework-5.3.0.24.zip",
-           checksum: "701c93afc4a120ae4d13fb1c4cbcfec24cef04828b5c67d2d40e9615e704f761"
+           checksum: "1714efefc4e46f2e2df0f67bc71421f42295ae7bb59537d40e59b928a080b175"
        ),
        
        .binaryTarget(
            name: "GIDSDK",
-           url: "https://nexus.gid.team/repository/sso-sdk-ios/gid/sdk/spm/GIDSDK-public-spm.xcframework-2.0.0.zip",
-           checksum: "03d98c260977157f982acbac75108525155161c2e4b804f908d3f0374e4ff99e"
+           url: "https://nexus.gid.team/repository/sso-sdk-ios/gid/sdk/spm/GIDSDK-public-spm.xcframework-2.0.1.zip",
+           checksum: "ba9e9cfcdc4a63fe100012f134875242ea65cf9facffe7481f1a7a6273765de4"
        ),
    ]
 )
