afaf31f9f99a4e6b13afc6b920928204c379c5f1
Бушуев Михаил Андреевич <mabushuev@gid.ru>
2026-05-22 11:20:37 +0300

    feat(SDK-2622): update name

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
        ),
    ],
 
-//    dependencies: [
-//        .package(url: "https://github.com/segmentio/analytics-swift.git", from: "1.8.0"),
-//        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.53.2")
-//    ],
+   dependencies: [
+       .package(url: "https://github.com/segmentio/analytics-swift.git", from: "1.8.0"),
+       .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.53.2")
+   ],
 
    targets: [
        .target(
@@ -29,21 +29,21 @@ let package = Package(
            path: "SwiftPM/GIDSDKTarget"
        ),
 
-    //    .target(
-    //         name: "GIDSDKDependencies",
-    //         dependencies: [
-    //             .product(name: "Segment", package: "analytics-swift"),
-    //             .product(name: "Sentry", package: "sentry-cocoa")
-    //         ],
-    //         path: "SwiftPM/GIDSDKDependencies"
-    //     ),
+       .target(
+            name: "GIDSDKDependencies",
+            dependencies: [
+                .product(name: "Segment", package: "analytics-swift"),
+                .product(name: "Sentry", package: "sentry-cocoa")
+            ],
+            path: "SwiftPM/GIDSDKDependencies"
+        ),
        
        .target(
            name: "GIDSDKWrapper",
            dependencies: [
-               .target(name: "GIDSDK", condition: .when(platforms: [.iOS, .tvOS])),
+               .target(name: "GIDKit", condition: .when(platforms: [.iOS, .tvOS])),
                .target(name: "KFPMobileKit", condition: .when(platforms: [.iOS, .tvOS])), 
-            //    .target(name: "GIDSDKDependencies", condition: .when(platforms: [.iOS, .tvOS])), 
+               .target(name: "GIDSDKDependencies", condition: .when(platforms: [.iOS, .tvOS])), 
             //    .product(name: "Segment", package: "analytics-swift"),
             //    .product(name: "Sentry", package: "sentry-cocoa")
            ],
@@ -57,7 +57,7 @@ let package = Package(
        ),
        
        .binaryTarget(
-           name: "GIDSDK",
+           name: "GIDKit",
            url: "https://nexus.gid.team/repository/sso-sdk-ios/gid/sdk/spm/GIDSDK-public-spm.xcframework-2.0.1.zip",
            checksum: "ba9e9cfcdc4a63fe100012f134875242ea65cf9facffe7481f1a7a6273765de4"
        ),
